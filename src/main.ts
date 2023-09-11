9bd5fa8384a97371639e4c47833d51dd1c2a568b
Jose Leles <jose.leles@eshows.com.br>
2023-09-11 18:28:17 -0300

    wip nest microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices/enums';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const appApi = await NestFactory.create(AppModule);
+  await appApi.listen(3000);
+
+  const appMicroService =
+    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:2181'],
+        },
+      },
+    });
+  // appMicroService.listen();
 }
 bootstrap();
