0620e82164c72eabb968387a6dc271003c389ce0
Jose Leles <jose.leles@eshows.com.br>
2023-09-12 10:12:36 -0300

    sending and receiving

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  Client,
+  ClientKafka,
+  KafkaOptions,
+  MessagePattern,
+  Payload,
+} from '@nestjs/microservices';
+import { kafka } from './kafka';
 
 @Controller()
 export class AppController {
+  @Client(kafka as KafkaOptions)
+  client: ClientKafka;
+
   constructor(private readonly appService: AppService) {}
+
+  async onModuleInit() {
+    this.client.subscribeToResponseOf('teste_topic');
+    await this.client.connect();
+  }
 
   @Get()
   getHello(): string {
+    this.client.emit('teste_topic', 'hello');
     return this.appService.getHello();
+  }
+
+  @MessagePattern('teste_topic')
+  processTopic(@Payload() message: any): any {
+    console.log(message);
+    return message;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions } from '@nestjs/microservices';
-import { Transport } from '@nestjs/microservices/enums';
+import { kafka } from './kafka';
 
 async function bootstrap() {
   const appApi = await NestFactory.create(AppModule);
   await appApi.listen(3000);
 
   const appMicroService =
-    await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-      transport: Transport.KAFKA,
-      options: {
-        client: {
-          brokers: ['localhost:2181'],
-        },
-      },
-    });
-  // appMicroService.listen();
+    await NestFactory.createMicroservice<MicroserviceOptions>(
+      AppModule,
+      kafka as MicroserviceOptions,
+    );
+  appMicroService.listen();
 }
 bootstrap();
